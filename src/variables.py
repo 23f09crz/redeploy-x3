ceb390b1ad24bf9a65df7e6c36d5cb668175d645
23f09crz <151797285+23f09crz@users.noreply.github.com>
2024-07-21 14:30:24 -0300

    add standings

--- src/variables.py
+++ src/variables.py
@@ -3,6 +3,7 @@ import streamlit as st
 live_games_api_url = st.secrets["api_football"]["live_games_api_url"]
 team_history_api_url = st.secrets["api_football"]["team_history_api_url"]
 fixture_statistics_api_url = st.secrets["api_football"]["fixture_statistics_api_url"]
+standings_api_url = st.secrets["api_football"]["standings_api_url"]
 headers = {
     "x-rapidapi-key": st.secrets["api_football"]["x_rapidapi_key"],
     "x-rapidapi-host": st.secrets["api_football"]["x_rapidapi_host"]
